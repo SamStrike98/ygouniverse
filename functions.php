d4b074d9f41c3d100471bb5f9d026610eb327879
Sam Strike <sam.strike1@gmail.com>
2025-05-20 19:56:58 +0100

    Added tailwind

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+function ygouniverse_enqueue_styles()
+{
+    wp_enqueue_style('tailwind', get_template_directory_uri() . '/src/output.css');
+};
+
+add_action('wp_enqueue_scripts', 'ygouniverse_enqueue_styles');
+
+register_nav_menus(
+    array(
+        'ygouniverse_main_menu' => 'Main Menu',
+        'ygouniverse_footer_menu' => 'Footer Menu'
+    )
+);
+add_theme_support('post-thumbnails');
+add_theme_support('custom-logo', array('width' => 200, 'height' => 100, 'flex-height' => true, 'flex-width' => true));
