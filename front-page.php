b8f4837d803afdd5773a0581b95de4e6d557676e
Sam Strike <sam.strike1@gmail.com>
2025-05-29 21:49:51 +0100

    Created latest posts template part & added social icons.

--- front-page.php
+++ front-page.php
@@ -8,52 +8,43 @@
 
 
         <div class="h-[200px] grid grid-cols-1 md:grid-cols-3 xl:grid-cols-6 justify-items-center gap-3">
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Format Guides</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Product Guides</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Anime</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">News</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Tournaments</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Master Duel</div>
+            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Format Guides</div>
+            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Product Guides</div>
+            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Anime</div>
+            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">News</div>
+            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Tournaments</div>
+            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Master Duel</div>
         </div>
     </div>
 
-    <div class="my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] px-3 py-10 text-[#DFD0B8]">
-        <h3>Latest Posts:</h3>
-        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-6 w-full mt-10">
-            <?php
-            // WordPress loop to get 3 latest posts
-            $args = array(
-                'post_type' => 'post',
-                'posts_per_page' => 3
-            );
-            $latest_posts = new WP_Query($args);
+    <?php get_template_part('template-parts/posts', 'latest'); ?>
 
-            if ($latest_posts->have_posts()) :
-                while ($latest_posts->have_posts()) : $latest_posts->the_post(); ?>
-                    <?php get_template_part('template-parts/post', 'item'); ?>
-            <?php endwhile;
-                wp_reset_postdata();
-            else :
-                echo '<p>No recent posts found.</p>';
-            endif;
-            ?>
+
+    <div class="flex flex-row gap-8">
+        <div class="h-[200px] my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-between items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">
+            <h3 class="">Newsletter:</h3>
+
+            <form class="flex flex-col items-center gap-5">
+                <div class="flex flex-row gap-8">
+                    <label class="font-ygo text-2xl">Email:</label>
+                    <input class="w-[300px]" placeholder="email">
+                </div>
+                <button>Sign Up</button>
+            </form>
         </div>
-    </div>
 
-    <div class="my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">
-        <h3>Newsletter:</h3>
-
-        <form>
-
-            <div>
-                <label>Email</label>
-                <input>
+        <div class="h-[200px] my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col gap-16 items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">
+            <h3 class="">Follow Us:</h3>
+            <div class="bg-green w-full flex flex-row justify-evenly">
+                <i class="fa-brands fa-x-twitter fa-2xl cursor-pointer"></i>
+                <i class="fa-brands fa-facebook-f fa-2xl cursor-pointer"></i>
+                <i class="fa-brands fa-instagram fa-2xl cursor-pointer"></i>
+                <i class="fa-brands fa-youtube fa-2xl cursor-pointer"></i>
+                <i class="fa-brands fa-twitch fa-2xl cursor-pointer"></i>
             </div>
-
-            <button>Sign Up</button>
-
-        </form>
+        </div>
     </div>
+
 
 
 
