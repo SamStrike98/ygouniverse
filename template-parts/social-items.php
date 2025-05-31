1cfe84aa0c76e47774e0b48c4e0f96fa5e943a8b
Sam Strike <sam.strike1@gmail.com>
2025-05-31 16:33:18 +0100

    Made changes to the newsletter form.

--- template-parts/social-items.php
+++ template-parts/social-items.php
@@ -1,8 +1,8 @@
 <div class="flex flex-row gap-8">
-    <div class="h-[200px] my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-between items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">
+    <div class="h-[200px] my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-between items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-8 text-[#DFD0B8]">
         <h3 class="">Newsletter:</h3>
 
-        <form class="flex flex-col items-center gap-5">
+        <form class="flex flex-row items-center gap-5">
             <div class="flex flex-row gap-8">
                 <label class="font-ygo text-2xl">Email:</label>
                 <input class="w-[300px]" placeholder="email">
@@ -11,7 +11,7 @@
         </form>
     </div>
 
-    <div class="h-[200px] my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col gap-16 items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">
+    <div class="h-[200px] my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col gap-16 items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-8 text-[#DFD0B8]">
         <h3 class="">Follow Us:</h3>
         <div class="bg-green w-full flex flex-row justify-evenly">
             <i class="fa-brands fa-x-twitter fa-2xl cursor-pointer"></i>
