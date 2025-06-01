28f90b1ebb85855a0f603f0315a94a9354eb2b26
Sam Strike <sam.strike1@gmail.com>
2025-06-01 20:47:36 +0100

    Added hamburger & cross icons.

--- header.php
+++ header.php
@@ -11,24 +11,26 @@
 <body class="bg-[#222831]">
     <header class="bg-[#222831] h-[100px] pt-5">
         <nav class="flex flex-row justify-between container mx-auto">
-        <div class="logo">
-            <?php
-            if (has_custom_logo()) {
-                the_custom_logo();
-            } else { ?>
-                <a href="<?php echo home_url('/'); ?>"><span class="text-[#DFD0B8] font-ygo text-6xl"><?php bloginfo('name') ?></span></a>
-            <?php }
-            ?>
+            <div class="logo">
+                <?php
+                if (has_custom_logo()) {
+                    the_custom_logo();
+                } else { ?>
+                    <a href="<?php echo home_url('/'); ?>"><span class="text-[#DFD0B8] font-ygo text-6xl"><?php bloginfo('name') ?></span></a>
+                <?php }
+                ?>
 
-        </div>
-        <div class="w-full flex flex-row justify-end">
-        <?php wp_nav_menu(array(
-            'theme_location' => 'ygouniverse_main_menu',
-            'menu_class' => 'space-y-2', // Ensure spacing
-            'container' => false, // Remove extra container div
-            'items_wrap' => '<ul class="mt-3 flex flex-row text-[#DFD0B8] text-3xl font-ygo font-bold gap-8">%3$s</ul>',
-        )); ?>
-        </div>
-    </nav>
+            </div>
+            <div class="w-full flex flex-row justify-end">
+                <?php wp_nav_menu(array(
+                    'theme_location' => 'ygouniverse_main_menu',
+                    'menu_class' => 'space-y-2', // Ensure spacing
+                    'container' => false, // Remove extra container div
+                    'items_wrap' => '<ul class="mt-3 flex flex-row text-[#DFD0B8] text-3xl font-ygo font-bold gap-8">%3$s</ul>',
+                )); ?>
+            </div>
+        </nav>
+        <i class="fa-solid fa-bars cursor-pointer text-[#DFD0B8] fa-2xl hover:text-[#948979] transition-colors"></i>
+        <i class="fa-solid fa-xmark cursor-pointer text-[#DFD0B8] fa-2xl hover:text-[#948979] transition-colors"></i>
     </header>
     <div class="min-h-[80vh] container mx-auto bg-[#222831]">
