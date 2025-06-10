5d1a6bccea99499a54972ca233939036e27287b9
Sam Strike <sam.strike1@gmail.com>
2025-06-10 07:35:31 +0100

    Adjusted height styling for small screen sizes.

--- front-page.php
+++ front-page.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 <div class="container mx-auto py-10">
-    <div class="h-[350px] flex flex-col justify-between">
+    <div class="min-h-[350px] flex flex-col justify-between">
         <div class="">
             <h1 class="text-center underline decoration-[#6c7482]">Welcome to YGO Universe</h1>
             <h4 class="text-center">All Things Yu-Gi-Oh!</h4>
         </div>
 
 
-        <div class="h-[200px] grid grid-cols-1 md:grid-cols-3 xl:grid-cols-6 justify-items-center gap-3">
+        <div class="min-h-[200px] grid grid-cols-1 md:grid-cols-3 xl:grid-cols-6 justify-items-center gap-3">
             <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Format Guides</div>
             <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Product Guides</div>
             <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Anime</div>
