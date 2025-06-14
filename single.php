2a584e64314e8f9812ccc44ad8d05fde99cd29c4
Sam Strike <sam.strike1@gmail.com>
2025-06-14 22:34:39 +0100

    Adjusted content padding for smaller screens.

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
 
             <article class="">
 
-                <div class="my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md h-[500px] w-full h-[200px] flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]"
+                <div class="my-10 bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full h-[400px] flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]"
                     style="background-image: url(<?php the_post_thumbnail_url(array(9999, 350)) ?>);">
                     <h1 class=""><?php the_title(); ?></h1>
                 </div>
@@ -17,7 +17,7 @@
                         </div>
                     </div>
                     <h4 class=""><?php the_category(); ?></h4>
-                    <div class="p-15"><?php the_content(); ?></div>
+                    <div class="p-0 md:p-15"><?php the_content(); ?></div>
                 </div>
             </article>
 
