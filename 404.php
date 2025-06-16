8b9bdf9d2029d98589fe40c9f2703d33dd96289c
Sam Strike <sam.strike1@gmail.com>
2025-06-16 15:10:29 +0100

    Return to homepage button.

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 
     <h1 class="text-center text-[#DFD0B8]">Page Not Found</h1>
 
-
+    <div class="text-center text-[#DFD0B8] text-2xl mt-10">Return to <a class="font-extrabold transition-colors hover:text-[#948979]" href="<?php echo home_url('/'); ?>">homepage</a></div>
 
 </div>
 <?php get_footer(); ?>
