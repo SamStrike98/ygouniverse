53f21a977e8e4e3b68489d41553d407d06b20141
Sam Strike <sam.strike1@gmail.com>
2025-06-16 15:11:33 +0100

    Added ACF page title and tagline

--- front-page.php
+++ front-page.php
@@ -1,21 +1,10 @@
-<?php get_header(); ?>
+<?php get_header();?>
 <div class="container mx-auto py-10">
     <div class="min-h-[350px] flex flex-col justify-between">
         <div class="py-10">
-            <h1 class="text-center underline decoration-[#6c7482]">Welcome to YGO Universe</h1>
-            <h4 class="text-center">All Things Yu-Gi-Oh!</h4>
+            <h1 class="text-center underline decoration-[#6c7482]"><?php echo esc_html(get_field('welcome_message')); ?></h1>
+            <h4 class="text-center"><?php echo esc_html(get_field('website_tag_line')); ?></h4>
         </div>
-
-
-        <!-- <div class="min-h-[200px] grid grid-cols-1 md:grid-cols-3 xl:grid-cols-6 justify-items-center gap-3">
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Format Guides</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Product Guides</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Anime</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">News</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Tournaments</div>
-            <div class="bg-[#393E46] border-[#DFD0B8] border-6 rounded-md w-full flex flex-col justify-center items-center shadow-[inset_0px_0px_15px_6px_rgba(0,_0,_0,_0.8)] p-3 text-[#DFD0B8]">Master Duel</div>
-        </div> -->
-
 
         <?php get_template_part('template-parts/frontpage', 'list'); ?>
     </div>
