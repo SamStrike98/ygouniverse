8242f73131535c4993c8e03ecae06324c842915c
Sam Strike <sam.strike1@gmail.com>
2025-06-17 19:02:39 +0100

    Created initial search file.

---
+++ search.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<div class="container mx-auto py-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-4">
+        <?php if (have_posts()): ?>
+            <?php while (have_posts()): the_post(); ?>
+                <?php get_template_part('template-parts/post', 'item'); ?>
+            <?php endwhile; ?>
+    </div>
+
+<?php else: ?>
+    <p>No Posts Found</p>
+<?php endif; ?>
+
+
+</div>
+<?php get_footer(); ?>
