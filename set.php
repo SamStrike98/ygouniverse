7b5ff159649e0a3eb7325c2f6c8a012e786fa352
Sam Strike <sam.strike1@gmail.com>
2025-06-18 22:25:18 +0100

    Added set list to set template.

---
+++ set.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<div class="container mx-auto py-10">
+
+    <?php get_template_part('template-parts/page', 'banner'); ?>
+
+    <?php get_template_part('template-parts/set', 'list'); ?>
+
+    <div class="text-[#DFD0B8]">
+        <p><?php the_content(); ?></p>
+    </div>
+
+
+</div>
+<?php get_footer(); ?>
