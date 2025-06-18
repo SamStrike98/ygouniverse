471a0216f6b048679de8e7023ad0a81d4ca13531
Sam Strike <sam.strike1@gmail.com>
2025-06-18 20:14:50 +0100

    Added copyright to footer.

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
-
 <?php get_template_part('template-parts/social', 'items'); ?>
 </div>
 <footer class="w-full h-[150px] bg-[#222831]">
+    <div class="h-full flex flex-row justify-between container mx-auto">
+        <p>©<?php bloginfo('name') ?></p>
+    </div>
 
 </footer>
 <?php wp_footer(); ?>
