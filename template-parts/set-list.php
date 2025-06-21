935294666d9e3a56bd9a0e57447f8021ab9c0897
Sam Strike <sam.strike1@gmail.com>
2025-06-21 20:57:12 +0100

    Added nested repeater field and made use of the set-item template part.

--- template-parts/set-list.php
+++ template-parts/set-list.php
@@ -1,45 +1,29 @@
-<?php
+<h2 class="text-center"><?php echo esc_html(get_field('type')); ?></h2>
+<div class="flex flex-col gap-15">
+    <?php
+    if (have_rows('year_list')):
+        while (have_rows('year_list')) : the_row();
+    ?>
 
-// Check rows exists.
-if (have_rows('set_list')):
-?>
-    <div class="flex flex-col gap-5">
-        <?php
-        // Loop through rows.
-        while (have_rows('set_list')) : the_row();
 
-            // Load sub field value.
-            $name = get_sub_field('name');
-            $code = get_sub_field('code');
-            $image = get_sub_field('image');
-            $release_date = get_sub_field('release_date');
-            $link_to_page = get_sub_field('link_to_page');
-        ?>
-            <div class="flex flex-row items-center gap-10">
-                <div class="flex flex-col">
-                    <h2><?php echo esc_html($name); ?></h2>
-                    <p><?php echo esc_html($code); ?></p>
-                    <p>Release Date: <?php echo esc_html($release_date); ?></p>
-                </div>
+            <div class="gap-15">
+                <h3 class="text-center my-5"><?php echo esc_html(get_sub_field('year')); ?></h3>
                 <?php
-                if (!empty($image)): ?>
-                    <figure class="overflow-hidden h-[200px] rounded-md">
-                        <img class="object-cover h-full transition-transform duration-300 group-hover:scale-100 rounded-t-md" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                    </figure>
-                <?php endif; ?>
+                // Loop over sub repeater rows.
+                if (have_rows('set_list')): ?>
+                    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-4">
+                        <?php while (have_rows('set_list')) : the_row();
 
-                <?php
-                if ($link_to_page): ?>
-                    <a class="button" href="<?php echo esc_url($link_to_page); ?>">More Details</a>
-                <?php endif; ?>
+                            get_template_part('template-parts/set', 'item'); 
+
+                        endwhile; ?>
+                    </div>
+                <?php endif;
+                ?>
             </div>
-        <?php
-        // End loop.
+
+    <?php
         endwhile;
-        ?>
-    </div>
-<?php
-// No value.
-else :
-// Do something...
-endif;
+    endif;
+    ?>
+</div>
