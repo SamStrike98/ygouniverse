4e9429d6ceea7466890c939b1b2b8b090125908d
Sam Strike <sam.strike1@gmail.com>
2025-06-29 21:36:05 +0100

    Grabbing the title and permalink to the formats.

--- template-parts/set-list.php
+++ template-parts/set-list.php
@@ -5,25 +5,29 @@
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-4">
             <?php foreach ($cards as $card): ?>
                 <div>
-                    <?php if($card->name): ?>
-                    <h3><?php echo $card->name; ?></h3>
-                    <?php $formats = $card->formats ?>
-                    <?php if($formats): ?>
-                        <?php foreach($formats as $format): ?>
-                            <?php echo $format ?>
-                        <?php endforeach; ?>
+                    <?php if ($card->name): ?>
+                        <h3><?php echo $card->name; ?></h3>
+                        <?php $formats = $card->formats ?>
+                        <?php if ($formats): ?>
+                            <?php foreach ($formats as $format): ?>
+                                <div>
+                                    <a href="<?php echo get_permalink($format); ?>">
+                                        <?php echo get_the_title($format); ?>
+                                    </a>
+                                </div>
+
+
+                            <?php endforeach; ?>
+                        <?php endif; ?>
                     <?php endif; ?>
-                    <?php endif;?>
                 </div>
             <?php endforeach; ?>
         </div>
-        <?php endif; ?>
+    <?php endif; ?>
 
-        <pre><?php print_r($cards); ?></pre>
+    <pre><?php print_r($cards); ?></pre>
 
-        </div>
-
-
+</div>
 
 
 
@@ -32,31 +36,33 @@
 
 
 
-        <div class="flex flex-col gap-15">
-            <?php
-            if (have_rows('year_list')):
-                while (have_rows('year_list')) : the_row();
-            ?>
 
 
-                    <div class="gap-15">
-                        <h3 class="text-center my-5"><?php echo esc_html(get_sub_field('year')); ?></h3>
-                        <?php
-                        // Loop over sub repeater rows.
-                        if (have_rows('set_list')): ?>
-                            <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-4">
-                                <?php while (have_rows('set_list')) : the_row();
+<div class="flex flex-col gap-15">
+    <?php
+    if (have_rows('year_list')):
+        while (have_rows('year_list')) : the_row();
+    ?>
 
-                                    get_template_part('template-parts/set', 'item');
 
-                                endwhile; ?>
-                            </div>
-                        <?php endif;
-                        ?>
+            <div class="gap-15">
+                <h3 class="text-center my-5"><?php echo esc_html(get_sub_field('year')); ?></h3>
+                <?php
+                // Loop over sub repeater rows.
+                if (have_rows('set_list')): ?>
+                    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-4">
+                        <?php while (have_rows('set_list')) : the_row();
+
+                            get_template_part('template-parts/set', 'item');
+
+                        endwhile; ?>
                     </div>
+                <?php endif;
+                ?>
+            </div>
 
-            <?php
-                endwhile;
-            endif;
-            ?>
-        </div>
+    <?php
+        endwhile;
+    endif;
+    ?>
+</div>
