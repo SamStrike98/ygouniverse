05dd0390aedee4193b994fd58f61f6b7ecc42fca
Sam Strike <sam.strike1@gmail.com>
2025-07-01 18:44:32 +0100

    Updated footer arrow.

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <?php get_template_part('template-parts/social', 'items'); ?>
 </div>
 <footer class="w-full h-[150px] bg-[#222831]">
-    <div class="h-full flex flex-row justify-between container mx-auto">
+    <div class="h-full flex flex-row justify-between items-end container mx-auto py-5">
         <p>©<?php bloginfo('name') ?></p>
-        <a href="#header" class=""><i class="fa-solid fa-arrow-up cursor-pointer text-[#DFD0B8] fa-2xl hover:text-[#948979] transition-colors"></i></a>
+        <a href="#header" class="border-[#DFD0B8] border-4 rounded-full p-2 hover:text-[#948979] transition-colors"><i class="fa-solid fa-arrow-up cursor-pointer text-[#DFD0B8] fa-2xl hover:text-[#948979] transition-colors"></i></a>
     </div>
 </footer>
 <?php wp_footer(); ?>
