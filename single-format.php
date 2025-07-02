eb06365c6ea900ddadb2906ca531a9125201abb2
Sam Strike <sam.strike1@gmail.com>
2025-07-02 19:47:56 +0100

    Created file to display formats.

--- single-format.php
+++ single-format.php
@@ -6,6 +6,7 @@
 
     <?php get_template_part('template-parts/page', 'banner'); ?>
 
+    <?php get_template_part('template-parts/format', 'list'); ?>
 
     <div class="text-[#DFD0B8]">
         <p><?php the_content(); ?></p>
