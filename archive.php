f3e6ebb5d7e317e582d6d137b46b6f20b5f664ff
Sam Strike <sam.strike1@gmail.com>
2025-07-04 22:22:02 +0100

    Added title to archive pages.

--- archive.php
+++ archive.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <div class="container mx-auto py-10">
+    <h2><?php post_type_archive_title(); ?></h2>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-items-center gap-4">
         <?php if (have_posts()): ?>
             <?php while (have_posts()): the_post(); ?>
