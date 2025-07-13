0e96ad05fb364997f8185f691364c1572574225f
Sam Strike <sam.strike1@gmail.com>
2025-07-13 21:19:20 +0100

    Created format template files.

---
+++ format-cardpool-template.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+Template Name: Format Cardpool Template 
+Template Post Type: format
+*/
+get_header(); ?>
+
+<div class="container mx-auto py-10">
+
+
+
+
+</div>
+<?php get_footer(); ?>
---
+++ format-decks-template.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+Template Name: Format Decks Template 
+Template Post Type: format
+*/
+get_header(); ?>
+
+<div class="container mx-auto py-10">
+
+
+
+
+</div>
+<?php get_footer(); ?>
