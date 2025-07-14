bedc37164edcf2f1b6dc26dcfdf006d5b1fd6557
Sam Strike <sam.strike1@gmail.com>
2025-07-14 09:44:09 +0100

    Added description field and date field.

--- format-about-template.php
+++ format-about-template.php
@@ -6,9 +6,10 @@ Template Post Type: format
 get_header(); ?>
 
 <div class="container mx-auto py-10">
+    <h2><?php the_title(); ?></h2>
 
-
-
+    <?php the_field('description'); ?>
+    <p>Event Date: <?php echo esc_html(get_field('format_date')); ?></p>
 
 </div>
 <?php get_footer(); ?>
