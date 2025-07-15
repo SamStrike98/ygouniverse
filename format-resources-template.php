9b99c3d9178fea39cee59da0d5caccd4865fa9fb
Sam Strike <sam.strike1@gmail.com>
2025-07-15 17:01:43 +0100

    Created format resources template page.

---
+++ format-resources-template.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+Template Name: Format Resources Template 
+Template Post Type: format
+*/
+get_header(); ?>
+
+<div class="container mx-auto py-10">
+    <h2><?php the_title(); ?></h2>
+
+    <?php the_field('description'); ?>
+    <p>Event Date: <?php echo esc_html(get_field('format_date')); ?></p>
+
+</div>
+<?php get_footer(); ?>
