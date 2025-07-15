545b5e4f23518e95c89f13731fb1c296a351a1a7
Sam Strike <sam.strike1@gmail.com>
2025-07-15 19:34:21 +0100

    Added code for resources repeater field.

--- format-resources-template.php
+++ format-resources-template.php
@@ -8,8 +8,36 @@ get_header(); ?>
 <div class="container mx-auto py-10">
     <h2><?php the_title(); ?></h2>
 
-    <?php the_field('description'); ?>
-    <p>Event Date: <?php echo esc_html(get_field('format_date')); ?></p>
+    <?php
+
+    // Check rows exists.
+    if (have_rows('resources')):
+
+        // Loop through rows.
+        while (have_rows('resources')) : the_row();
+
+            // Load sub field value.
+            $name = get_sub_field('resource_name');
+            $type = get_sub_field('resource_type');
+            $link = get_sub_field('resource_link');
+            $file = get_sub_field('resource_file');
+
+            if ($type == "website"): ?>
+                <?php
+                if ($link): ?>
+                    <a class="button" href="<?php echo esc_url($link); ?>"><?php echo $name; ?></a>
+                <?php endif;
+            else:
+                if ($file): ?>
+                    <a href="<?php echo $file['url']; ?>" download><?php echo $name; ?></a>
+    <?php endif;
+            endif;
+        // Do something, but make sure you escape the value if outputting directly...
+
+        // End loop.
+        endwhile;
+    endif;
+    ?>
 
 </div>
 <?php get_footer(); ?>
